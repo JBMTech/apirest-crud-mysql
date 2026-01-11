8433ed7af05190c7d54b6a7c63c552aad7d69751
Jairo <jairo.buleje97@gmail.com>
2026-01-11 15:11:09 +0100

    first commit

---
+++ src/main/java/com/example/apirest/proyect/model/university.java
@@ -0,0 +1,22 @@
+package com.example.apirest.proyect.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class university {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+}
